5bb8367b528199ffdd5380957fab1db86076e1b7
Md. Nazmul Islam Rumman <142523457+pomegstar@users.noreply.github.com>
2024-12-25 13:52:48 +0000

    initial push

--- finaid/finaid/settings.py
+++ finaid/finaid/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 MEDIA_URL = '/media/'  # URL to access media files via the browser
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Root directory for media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
